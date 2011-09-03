2ab9df0f5ce3ba81942c6ce41cdcba7beb868a75
Goro Tsukiyama <goro.tsukiyama@gmail.com>
2011-09-04 01:03:43 +0900

    add new headerfile from 32

---
+++ kernel/include/linux/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 20 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#20 PREEMPT Sun Sep 4 01:02:07 JST 2011"
+#define LINUX_COMPILE_TIME "01:02:07"
+#define LINUX_COMPILE_BY "Goro"
+#define LINUX_COMPILE_HOST "Tsukiyama"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.5.2 (Sourcery G++ Lite 2011.03-42) "
